f1585e446907bc99eb8560a801bcde6469679621
AldipB <boharaaldip8529@gmail.com>
2026-04-29 16:35:08 -0400

    fix stripe type error

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -27,7 +27,7 @@ export async function POST(req: Request) {
   }
 
   if (event.type === "checkout.session.completed") {
-    const session = event.data.object as Stripe.CheckoutSession;
+    const session = event.data.object as Stripe.Checkout.Session;
     const email = session.customer_email;
 
     if (email) {
